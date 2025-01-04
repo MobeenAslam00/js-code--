812180893845ec58dbde45a92489a71ef120a114
MobeenAslam00 <105664539+MobeenAslam00@users.noreply.github.com>
2025-01-04 10:28:50 +0000

    datatype

---
+++ 01-basics/datatype.js
@@ -0,0 +1,16 @@
+"use strict"; // treat jscode as new version
+// Alert
+//alert(3+3) give error bcz its node js engine not browser
+/*
+Types
+num -> 2 to power 53
+bigint
+string =""
+boolean = true/ false
+null= standalone value    While Null is object
+undefined= just variable creat but no value assigned   Basically its undefined
+symbol= unique
+object
+
+*/
+console.log(typeof null )
---
+++ 01-basics/variable.js
@@ -0,0 +1,11 @@
+// const,let,var, without specifying any type
+const name="Mobin"
+var email="bintesajida77@gmail.com"
+let uni="PUCIT"
+dept="DSE"
+console.table([ name, email, uni, dept])
+/*
+use const for fix value, use let mostly avoid var bcz it can change the value bcz of its scope while let have only block scoped.
+without specifying any type , we can assign variable.
+without assignig any value to variable shows undefined status.
+*/ 
